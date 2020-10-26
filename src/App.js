b22e9eefcc297be224d70600190206a2d254ce2a
giuliano00 <giuliano.botto@davinci.edu.ar>
2020-10-25 21:50:42 -0300

    no muesta en web

--- src/App.js
+++ src/App.js
@@ -1,18 +1,108 @@
+  
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-
 class App extends React.Component{
 
-  componentDidMount(){
-    axios.get('https://kitsu.io/api/edge/characters/1').then(result=>{console.log(result.data)}).catch(console.log)
+state = {
+response: [],
+estado: null
 
-  }
-  render(){
-    return(<h1>kitsu api</h1>);
-    
+}
+
+handlerBuscar(t){
+
+  var buscar = t.target.value;
+  this.setState({value : buscar });
+
+}
+
+handlerCLick = () =>{
+
+  var buscar = this.state.value;
+
+ if(buscar == null ){
+  axios.get("https://kitsu.io/api/edge/characters/"
+  )
+  .then(
+    dato => {
+      console.log( dato.data);
+
+      this.setState({
+        response: dato.data,
+        estado: true
+      })
+      console.log(this.state)
+
+    });
+
+ }else{
+
+  axios.get("https://kitsu.io/api/edge/characters/"+buscar)
+  .then(
+    dato => {
+      console.log( dato.data);
+
+      this.setState({
+        response: dato.data,
+        estado: true
+      })
+      console.log(this.state)
+
+    });
   }
 }
+
+handlerCLickRandom = () =>{
+
+  axios.get("https://kitsu.io/api/edge/characters/")
+  .then(
+    dato => {
+      console.log( dato.data);
+
+      this.setState({
+        response: dato.data,
+        estadoR: true
+      })
+      console.log(this.state)
+    });
+}
+  render() {
+    if(this.state.estado != true && this.state.estadoR != true){
+      return (
+        <div className="App">
+          <header className="App-header">
+           
+          <div>
+            <h2>Buscar un personaje por id</h2>
+            <input type="text" name="name" pattern="[a-z]{1,15}" onChange={this.handlerBuscar.bind(this)}/>
+            <input type="button" value="Buscar" onClick={this.handlerCLick.bind(this)}/>
+          </div>
+
+         
+          </header>
+        </div>
+      );
+
+      }else if(this.state.estado == true) {
+      return (
+        <div className="App">
+          <header className="App-header">
+           
+          <div>
+            <h2>Buscar otro personaje por id</h2>
+            <input type="text" name="name" pattern="[a-z]{1,15}" onChange={this.handlerBuscar.bind(this)}/>
+            <input type="button" value="Buscar" onClick={this.handlerCLick.bind(this)}/>
+
+          </div>
+          </header>
+        </div>
+      );
+  }
+}
+}
+
 export default App;
+
